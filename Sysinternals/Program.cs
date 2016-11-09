0d17fb23a0e7f9fc85b10748ca880b96658bc52e
proengin <robmccullough13@gmail.com>
2016-11-09 10:20:54 -0500

    inital commit

---
+++ Sysinternals/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sysinternals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Microsoft.Win32.RegistryKey key = Microsoft.Win32.Registry.CurrentUser.OpenSubKey("SOFTWARE", true);
+            var sysinternalKey = key.CreateSubKey("Sysinternals");
+            var syncKey = sysinternalKey.CreateSubKey("Sync");
+            syncKey.SetValue("EulaAccepted", 0x01, Microsoft.Win32.RegistryValueKind.DWord);
+            syncKey.Close();
+            sysinternalKey.Close();
+            key.Close();
+        }
+    }
+}
